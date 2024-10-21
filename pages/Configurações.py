947843cbd6c8ca5abe7336301c5524c994b12e99
juanpythonvolei <juanzsalca@outlook.com>
2024-10-20 22:53:24 -0300

    Update Configurações.py

--- pages/Configurações.py
+++ pages/Configurações.py
@@ -13,17 +13,23 @@ if admin_usuario and senha_admin:
     if admin_usuario == 'Juan' and int(senha_admin) == senha:
         selecao = st.selectbox(label='Seleção',placeholder="Selecione uma opção",options=['Alterar Usuário','Alterar Senha','Excluir Usuário'],index=None)
         if selecao == 'Alterar Usuário':
-            usuario_atual = st.text_input(label="",placeholder="Insira seu usuário atual")
-            novo_usuario= st.text_input(label="",placeholder="Insira seu novo usuário")
-            if usuario_atual and novo_usuario:
-                st.info(update_user(usuario_atual,'nome',novo_usuario))
+            alt_usuario = st.popover('alterar')
+            with alt_usuario:
+                usuario_atual = st.text_input(label="",placeholder="Insira seu usuário atual")
+                novo_usuario= st.text_input(label="",placeholder="Insira seu novo usuário")
+                if usuario_atual and novo_usuario:
+                    st.info(update_user(usuario_atual,'nome',novo_usuario))
         elif selecao == 'Alterar Senha':
-            usuario_atual = st.text_input(label="",placeholder="Insira seu usuário atual")
-            senha_atual= st.text_input(label="",placeholder="Insira sua senha atual")
-            nova_senha= st.text_input(label="",placeholder="Insira sua nova senha")
-            if usuario_atual and senha_atual and nova_senha:
-                st.info(update_user(usuario_atual,'senha',nova_senha))
+            alt_senha = st.popover('alterar')
+            with alt_senha:
+                usuario_atual = st.text_input(label="",placeholder="Insira seu usuário atual")
+                senha_atual= st.text_input(label="",placeholder="Insira sua senha atual")
+                nova_senha= st.text_input(label="",placeholder="Insira sua nova senha")
+                if usuario_atual and senha_atual and nova_senha:
+                    st.info(update_user(usuario_atual,'senha',nova_senha))
         elif selecao == 'Excluir Usuário':
-            usuario_atual = st.text_input("Selecione o usuário para a exclusão")
-            if usuario_atual:
-                st.info(delete_user(usuario_atual))
+            excl_usuario = st.popover('excluir')
+            with excl_usuario:
+                usuario_atual = st.text_input("Selecione o usuário para a exclusão")
+                if usuario_atual:
+                    st.info(delete_user(usuario_atual))
